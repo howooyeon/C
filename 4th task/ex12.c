c55bbf890670ddf609440ee86579739226f22491
howooyeon <aviationlovers@naver.com>
2021-09-20 20:28:33 +0900

    C++ 프로그래밍기초 실습

---
+++ 4th task/ex12.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int a, b;
+	char ch;
+
+	printf(" 첫번째 수를 입력하세요 : ");
+	scanf("%d", &a);
+	printf(" 계산할 연산자를 입력하세요 : ");
+	scanf(" %c", &ch);
+	printf(" 두번째 수를 입력하세요 : ");
+	scanf("%d", &b);
+
+	if (ch == '+')
+		printf("%d + %d = %d 입니다.\n", a, b, a + b);
+
+	else if (ch == '-')
+		printf("%d - %d = %d 입니다.\n", a, b, a - b);
+
+	else if (ch == '*')
+		printf("%d * %d = %d 입니다.\n", a, b, a * b);
+
+	else if (ch == '/')
+		printf("%d / %d = %f 입니다.\n", a, b, a/(float)b);
+
+	else if (ch == '-')
+		printf("%d %% %d = %d 입니다.\n", a, b, a % b);
+}
